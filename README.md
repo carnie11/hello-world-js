aaf5fd0d85de22be480cc36fd72e55aeee9cf585
carnie11 <carolin.modigh@gmail.com>
2022-11-15 08:42:53 +0100

    Övning gjord

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # hello-world-js
 Övning v 45
+
+
+
+--Krav till Hello World övning--
+
+<body> i din html sida skall vara tom. Förutom en <script> länk som hänvisar till din javascript fil.
+Med Javascript så skall vi till <body> skapa en <h1> med en rubrik till sidan (tex ditt namn).
+Sedan skall du lägga till minst en <p> med någon kort information (Tex en kort beskrivning av dig).
+Till sist skall du skapa en lista <ul> med minst 3 items <li> med valfritt innehåll (tex 3 saker du är bra på).
+Från din javascript fil skall du även skicka ut texten “Hello World” till konsollen.
---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+let newh1 = document.createElement('h1');
+newh1.innerHTML = "Carolin Nielsen";
+document.body.appendChild(newh1);
+
+let par = document.createElement('p');
+document.body.appendChild(par);
+par.innerHTML = "Jag heter Carolin och bor i Järfälla.";
+
+let lista = document.createElement('ul');
+document.body.appendChild(lista);
+
+let punkt = document.createElement('li');
+document.body.appendChild(punkt);
+punkt.innerHTML = "Bamberi";
+
+let punkt1 = document.createElement('li');
+document.body.appendChild(punkt1);
+punkt1.innerHTML = "Vila";
+
+let punkt2 = document.createElement('li');
+document.body.appendChild(punkt2);
+punkt2.innerHTML = "Äta";
+
+
+
